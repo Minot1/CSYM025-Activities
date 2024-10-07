7e14db04f5d710a5f363683888760092f87ffba7
Taner Dincer <dincer.taner123@gmail.com>
2024-10-07 16:37:58 +0100

    Added extra comments

--- test/TestWeek2.java
+++ test/TestWeek2.java
@@ -10,13 +10,14 @@ public class TestWeek2 {
     public void testNameGreeter() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(baos);
-        // Change system output to ps so that print statements will instead print to here
+        // Change system output to ps so that print statements will instead print here
         System.setOut(ps);
 
         NameGreeter nameGreeter = new NameGreeter();
+
+        // Simulate user input
         InputStream sysInBackup = System.in;
         ByteArrayInputStream in = new ByteArrayInputStream("Taner".getBytes());
-        // Simulate user input
         System.setIn(in);
         nameGreeter.read(System.in);
 
@@ -25,7 +26,7 @@ public class TestWeek2 {
         System.setIn(sysInBackup);
 
         nameGreeter.greet();
-        expected += " \nHello Taner";
+        expected += " \nHello Taner"; // Add the new expected output to the previous one
 
         // Trims and replaces are to get rid of EOF difference of Windows (LF to CRLF)
         assertEquals(expected.trim().replace("\r", ""), baos.toString().trim().replace("\r", ""));
